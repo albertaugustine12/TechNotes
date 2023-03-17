8501a2e9e70eb4d3fe608669a9e3ac405aa9356a
albertaugustine12 <albertaugustine@shilpas-mbp.lan>
2023-03-16 19:32:01 -0700

    changes to deploy 1

--- website/docs/Documentations/Markdown.md
+++ website/docs/Documentations/Markdown.md
@@ -1,4 +1,5 @@
 ---
+id: Markdown
 description: Tips and tricks for writing markdown files
 sidebar_position: 1
 ---
--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -20,6 +20,7 @@ const config = {
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'albertaugustine12', // Usually your GitHub org/user name.
   projectName: 'TechNotes', // Usually your repo name.
+  // deploymentBranch: 'gh-pages',
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
